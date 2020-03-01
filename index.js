3d6603436827293d4efbe33cfda2e187d3c901b1
paulomoisessilva <33613206+paulomoisessilva@users.noreply.github.com>
2020-03-01 20:39:21 -0300

    update

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
+
+
 function sucesso(x) {
-     console.log(x.login)
+    $("#loading").hide()
+     if(x.login){
+        $.get("conteudo.html", function(data){
+            if($("#sessão").is(':checked')){
+                sessionStorage.setItem("key", "logado");
+                sessionStorage.setItem("nome", x.dados[0][1]);
+            }
+            $(".card").css("height","460px")
+            $("#conteudo").html(data);
+            var nome = sessionStorage.getItem("nome") || x.dados[0][1]
+            $("#consultor").html("Consultor: "+nome)
+            $("#conteudo").show()
+        });
+        
+    }else{
+        $("#login").show()
+     }
+}
+
+function sair(e){
+    e.preventDefault()
+    $("#conteudo").html("")
+    $("#conteudo").hide()
+    $("#login").show()
+    $(".card").css("height","300px")
+    sessionStorage.removeItem("key");
+    sessionStorage.removeItem("nome");
 }
 
 function entrar(e) {
     e.preventDefault()
     var user = $("#user")[0].value
-    console.log(user)
+    $("#user")[0].value= ""
     if (user) {
-        var data = { func: "login", user: user }
-        console.log(data)
+        var data = JSON.stringify({ func: "login", user: user })
         $("#login").hide()
         $("#loading").show()
 
---
+++ session.js
@@ -0,0 +1,20 @@
+$(function(){
+    var logado = sessionStorage.getItem("key")
+    
+    if(logado !== null){
+        $.get("conteudo.html", function(data){
+            $(".card").css("height","460px")
+            $("#conteudo").html(data);
+            var nome = sessionStorage.getItem("nome")
+            $("#consultor").html("Consultor: "+nome)
+            $("#conteudo").show()
+        })
+        
+        $("#conteudo").show()
+        $("#loading").hide()
+    }else{
+        console.log("bbbbbb")
+         $("#loading").hide()
+         $("#login").show()
+    }
+})
